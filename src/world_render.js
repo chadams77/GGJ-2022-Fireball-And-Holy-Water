af6733f81d3f483dcc88ff3c023c2b3e7c39ed6a
Chris Adams <chris.ah.adams@gmail.com>
2022-01-22 14:15:56 -0400

    tree, stone block, cave block

--- src/world_render.js
+++ src/world_render.js
@@ -28,7 +28,7 @@ window.WorldRender = function(parent) {
             #define RM_MAX_DIST 10.
             #define RM_MAX_STEPS 100
             #define RM_MIN_DIST 0.01
-            #define _R_SEED vec2(${Math.random()*1000}, ${Math.random()*1000})
+            #define _R_SEED vec3(${Math.random()*1000}, ${Math.random()*1000}, ${Math.random()*1000})
 
             uniform vec2 res;
             uniform float time;
@@ -42,21 +42,78 @@ window.WorldRender = function(parent) {
             ${SDF_SHADERS}
 
             float SDF_StoneBlock (vec3 p, vec3 loc) {
-                return sdBoxSigned(p, loc, vec3(0.5)) + snoise(p*13.3) * 0.01;
+                return sdBoxSigned(p, loc, vec3(0.5, 0.5, 1.5)) + snoise(p*13.3) * 0.01;
             }
-            vec3 CLR_StoneBlock (vec3 p) {
+            vec3 CLR_StoneBlock (vec3 p, vec3 loc) {
                 return mix(vec3(0.2, 0.2, 0.2), vec3(0.1, 0.4, 0.15), pow(-snoise(p*13.3)*0.5+0.5, 3.));
+            }
+
+            float SDF_Cave1 (vec3 p, vec3 loc) {
+                return opSubtraction(sdBoxSigned(p, loc, vec3(0.5, 0.5, 1.5)), sdBoxSigned(p, loc, vec3(0.7, 0.7, 0.5))) + snoise(p*13.3) * 0.02;
+            }
+            vec3 CLR_Cave1 (vec3 p, vec3 loc) {
+                return mix(vec3(0.2, 0.2, 0.2), vec3(0.1, 0.4, 0.15), pow(-snoise(p*13.3)*0.5+0.5, 3.)) * 0.5;
+            }
+
+            float SDF_Grass (vec3 p, vec3 loc) {
+                return sdBoxSigned(p, loc + vec3(0., 0., 0.5), vec3(0.5)) + snoise(p*17.3) * 0.015;
+            }
+            vec3 CLR_Grass (vec3 p, vec3 loc) {
+                return mix(vec3(0.1, 0.4, 0.15)*0.2, vec3(0.1, 0.4, 0.15), pow(-snoise(p*17.3)*0.5+0.5, 3.));
+            }
+
+            float SDF_Tree_L3 (vec3 p, vec3 loc, float _r, float a) {
+                float r0 = rand(loc.xy + vec2(_r));
+                float r = a + (r0 - 0.5) * PI * 0.5;
+                float rad = (rand(loc.xy + vec2(a)) * 0.7 + 0.3) * 0.35;
+                vec3 loc2 = loc + normalize(vec3(cos(r) * 0.15, sin(r) * 0.15, -0.15)) * rad;
+                return sdTaperedLine(loc, loc2, 0.01, 0.0025, p);
+            }
+
+            float SDF_Tree_L2 (vec3 p, vec3 loc, float _r, float a) {
+                float r0 = rand(loc.xy + vec2(_r));
+                float r = a + (r0 - 0.5) * PI * 1.5;
+                float rad = (rand(loc.xy + vec2(a)) * 0.7 + 0.3) * 0.7;
+                vec3 loc2 = loc + normalize(vec3(cos(r) * 0.15, sin(r) * 0.15, -0.05)) * rad;
+                float dist = sdTaperedLine(loc, loc2, 0.025, 0.01, p);
+                dist = min(dist, SDF_Tree_L3(p, loc2, rand(vec2(r0, 4.)), r));
+                if (r0 > 0.25) {
+                    dist = min(dist, SDF_Tree_L3(p, loc2, rand(vec2(r0, 5.)), r));
+                }
+                return dist;
+            }
+
+            float SDF_Tree (vec3 p, vec3 loc) {
+                loc.z += 0.25;
+                float r0 = rand(loc.xy);
+                float r = r0 * PI * 2.;
+                vec3 loc2 = loc + vec3(cos(r) * 0.025, sin(r) * 0.025, -(0.2 + 0.1 * r0)/0.5);
+                float dist = sdTaperedLine(loc, loc2, 0.06, 0.025, p);
+                dist = min(dist, SDF_Tree_L2(p, loc2, rand(vec2(r0, 1.)), r));
+                dist = min(dist, SDF_Tree_L2(p, loc2, rand(vec2(r0, 2.)), r));
+                dist = min(dist, SDF_Tree_L2(p, loc2, rand(vec2(r0, 3.)), r));
+                dist = dist - (1. - worley(p*39.71, 3.7, false).x) * 0.02;
+                loc.z -= 0.25;
+                return min(dist, SDF_Grass(p, loc));
+            }
+            vec3 CLR_Tree (vec3 p, vec3 loc) {
+                if (SDF_Grass(p, loc) < RM_MIN_DIST) {
+                    return CLR_Grass(p, loc);
+                }
+                else {
+                    return mix(mix(vec3(0.325, 0.207, 0.03), vec3(0.325, 0.207, 0.03)*0.3, worley(p*39.71, 3.7, false).x), vec3(0.3, 0.3, 0.3), 0.5);
+                }
             }
 
             float sceneDist (vec3 p) {
                 return min(
                     min(
-                        SDF_StoneBlock(p, vec3(2., 0., 0.)),
-                        SDF_StoneBlock(p, vec3(-2., 0., 0.))
+                        SDF_Tree(p, vec3(2., 0., 0.)),
+                        SDF_Tree(p, vec3(-2., 0., 0.))
                     ),
                     min(
-                        SDF_StoneBlock(p, vec3(0., 6., 0.)),
-                        SDF_StoneBlock(p, vec3(0., -6., 0.))
+                        SDF_Cave1(p, vec3(0., 2., 0.)),
+                        SDF_Cave1(p, vec3(0., -2., 0.))
                     )
                 );
             }
@@ -75,17 +132,18 @@ window.WorldRender = function(parent) {
 
                 float diffuse = pow(max(dot(n, lightDir), 0.), 3.) * clamp(1. - dist / lightPos.w, 0., 1.);
 
-                if (SDF_StoneBlock(p, vec3(2., 0., 0.)) < RM_MIN_DIST) {
-                    ret = CLR_StoneBlock(p);
+                vec3 loc;
+                if (SDF_Tree(p, loc = vec3(2., 0., 0.)) < RM_MIN_DIST) {
+                    ret = CLR_Tree(p, loc);
                 }
-                else if (SDF_StoneBlock(p, vec3(-2., 0., 0.)) < RM_MIN_DIST) {
-                    ret = CLR_StoneBlock(p);
+                else if (SDF_Tree(p, loc = vec3(-2., 0., 0.)) < RM_MIN_DIST) {
+                    ret = CLR_Tree(p, loc);
                 }
-                else if (SDF_StoneBlock(p, vec3(0., 6., 0.)) < RM_MIN_DIST) {
-                    ret = CLR_StoneBlock(p);
+                else if (SDF_Cave1(p, loc = vec3(0., 2., 0.)) < RM_MIN_DIST) {
+                    ret = CLR_Cave1(p, loc);
                 }
-                else if (SDF_StoneBlock(p, vec3(0., -6., 0.)) < RM_MIN_DIST) {
-                    ret = CLR_StoneBlock(p);
+                else if (SDF_Cave1(p, loc = vec3(0., -2., 0.)) < RM_MIN_DIST) {
+                    ret = CLR_Cave1(p, loc);
                 }
 
                 ret += ret * lightColor * diffuse;
